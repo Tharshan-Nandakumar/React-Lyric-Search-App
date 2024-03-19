ca96d303b1a52fc56d1b9eecd75d79c6470719a7
Tharshan <tharshan1465@gmail.com>
2024-03-19 00:21:06 +0000

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import { Provider } from './context';
+//	900a86b72e736e60a11a0f18c98b5f15
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route exact path="/" element={<Index />} />
+            </Routes>
+          </div>
+        </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
-export default App;
+export default App
