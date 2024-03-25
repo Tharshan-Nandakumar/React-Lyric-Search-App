a199eec2a8ef8939d730ad924566a3543b7cbc89
Tharshan <tharshan1465@gmail.com>
2024-03-25 00:04:11 +0000

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+
 import { Provider } from './context';
 //	900a86b72e736e60a11a0f18c98b5f15
 
@@ -14,9 +16,10 @@ class App extends Component {
         <React.Fragment>
           <Navbar />
           <div className='container'>
-            <Routes>
-              <Route exact path="/" element={<Index />} />
-            </Routes>
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
+            </Switch>
           </div>
         </React.Fragment>
         </Router>
