a7c1219d641dc41aa6f3d2b03ebdc7386a7f97a0
Tharshan <tharshan1465@gmail.com>
2024-03-26 18:57:26 +0000

    context

--- src/context.js
+++ src/context.js
@@ -3,11 +3,24 @@ import axios from 'axios';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'SEARCH_TRACKS':
+      return {
+        ...state,
+        track_list: action.payload,
+        heading: 'Search Results'
+      };
+      default:
+        return state;
 
+  }
+}
 export class Provider extends Component {
     state = {
         track_list: [],
-        heading: 'Top 10 Tracks'
+        heading: 'Top 10 Tracks',
+        dispatch: action => this.setState(state => reducer(state, action))
     };
 
     componentDidMount() {
@@ -15,7 +28,7 @@ export class Provider extends Component {
             process.env.REACT_APP_MM_KEY
         }`)
             .then(res => {
-              console.log(res.data);
+              //console.log(res.data);
               this.setState({track_list: res.data.message.body.track_list})
             })
             .catch(err => console.log(err))
